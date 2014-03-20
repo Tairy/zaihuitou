1be867f5fb1a8e9fc45c3f50e1e2b490ae474527
Guo Gengrui <tairyguo@gmail.com>
2014-03-20 16:29:55 +0800

    Delete test.php

--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-date_default_timezone_set('Asia/Shanghai');
-
-echo date("Y-m-d", strtotime('friday next week',time()));
-// 
-// echo date('Y-n-d',strtotime('Monday'));
-// echo date('Y-n-d',strtotime('next Friday'));
-// echo date('Y-n-d',strtotime('last Monday'));
-// echo date('Y-n-d',strtotime('Friday'));
